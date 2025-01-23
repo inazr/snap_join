f48fefadb123826a8085d2f7d5f20a2a357cc573
Stephan Ohm <github@stephan-ohm.com>
2025-01-23 21:21:50 +0100

    added example core layer model core_snap__a_b_c.sql

---
+++ models/core/core_snap__a_b_c.sql
@@ -0,0 +1,6 @@
+{{ snap_join(['stg_seed_a', 'stg_seed_b','stg_seed_c'],
+             ['valid_from', 'valid_from', 'dbt_valid_from'],
+             ['valid_to', 'valid_to', 'dbt_valid_to'],
+             ['primary_key', 'primary_key', 'primary_key_with_another_name'],
+             [['dim_a', 'dim_b', 'dim_c'], ['dim_d', 'dim_e', 'dim_f'], ['dim_g', 'dim_h']]
+             ) }}
