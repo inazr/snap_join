4e313b97cf9fbce32a001ef4420cf890428ed2ca
Stephan Ohm <github@stephan-ohm.com>
2025-01-24 20:58:55 +0100

    renamed a parameter to prepare documentation

--- models/core/core_snap__a_b_c.sql
+++ models/core/core_snap__a_b_c.sql
@@ -1,7 +1,7 @@
 {{ snap_join(['stg_seed_a', 'stg_seed_b','stg_seed_c'],
              ['valid_from', 'valid_from', 'dbt_valid_from'],
              ['valid_to', 'valid_to', 'dbt_valid_to'],
-             ['primary_key', 'primary_key', 'primary_key_with_another_name'],
+             ['primary_key', 'this_is_not_a_primary_key', 'primary_key_with_another_name'],
              [['dim_a', 'dim_b', 'dim_c'], ['dim_d', 'dim_e', 'dim_f'], ['dim_g', 'dim_h']]
              ) }}
 
